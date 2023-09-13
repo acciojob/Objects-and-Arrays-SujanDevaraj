c2b95157ac3e560a786cf676159c381d50e314a2
SujanDevaraj <96370355+SujanDevaraj@users.noreply.github.com>
2023-09-13 21:01:19 +0530

    created a submission

--- script.js
+++ script.js
@@ -4,11 +4,22 @@ const person = {
   name: "John Doe",
   age: 80,
 };
+// Create a reference to the 'players' array
+const team = players;
 
+// Create a shallow copy of the 'players' array
+const team1 = [...players];
+
+// Create a shallow copy of the 'person' object
+const cap1 = { ...person };
+
+// Modify 'team1' and 'cap1' to demonstrate that they are copies
+team1[0] = "Mike";
+cap1.name = "Jane";
 // Write your code here, make sure the name of variables is exactly same as mentioned in the problem statement
 
 window.players = players;
 window.person = person;
-window.team = players;
-window.team1 = players;
-window.cap1 = person;
+window.team = team;
+window.team1 = team1;
+window.cap1 = cap1;
